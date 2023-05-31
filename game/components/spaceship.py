afeff325d6c10ed56ecacea22757a035c2f7ef59
JThiagoHM <jose.hinojosa@fundacion-jala.org>
2023-05-30 23:07:58 -0400

    Second-Class

--- game/components/spaceship.py
+++ game/components/spaceship.py
@@ -19,15 +19,7 @@ class Spaceship(Sprite):
        self.rect.y = self.Y_POS
 
    def update(self, user_input):
-       if user_input[pygame.K_LEFT]:
-         self.move_left()
-       elif user_input[pygame.K_RIGHT]:
-         self.move_right()
-       elif user_input[pygame.K_UP]:
-         self.move_up()
-       elif user_input[pygame.K_DOWN]:
-         self.move_down()
-       elif user_input[pygame.K_UP] and user_input[pygame.K_RIGHT]:
+       if user_input[pygame.K_UP] and user_input[pygame.K_RIGHT]:
          self.move_up_right()
        elif user_input[pygame.K_UP] and user_input[pygame.K_LEFT]:
          self.move_up_left()
@@ -35,20 +27,24 @@ class Spaceship(Sprite):
          self.move_down_right()
        elif user_input[pygame.K_DOWN] and user_input[pygame.K_LEFT]:
          self.move_down_left()
-
+       elif user_input[pygame.K_LEFT]:
+         self.move_left()
+       elif user_input[pygame.K_RIGHT]:
+         self.move_right()
+       elif user_input[pygame.K_UP]:
+         self.move_up()
+       elif user_input[pygame.K_DOWN]:
+         self.move_down()
+      
    def move_left(self):
-       if self.rect.left >= 0:
-         self.rect.x -= self.SHIP_SPEED
-       else:
-          if self.rect.left < 0:
-            self.rect.right = SCREEN_WIDTH
+      self.rect.x -= self.SHIP_SPEED
+      if self.rect.left < 0:
+        self.rect.x = SCREEN_WIDTH - self.SHIP_WIDTH
 
    def move_right(self):
-       if self.rect.right <= SCREEN_WIDTH:     
-         self.rect.x += self.SHIP_SPEED
-       else:
-          if self.rect.right > SCREEN_WIDTH:
-            self.rect.left = 0
+      self.rect.x += self.SHIP_SPEED
+      if self.rect.right >= SCREEN_WIDTH - self.SHIP_HEIGHT:
+        self.rect.x = 0
     
    def move_up(self):
        if self.rect.y > SCREEN_HEIGHT // 2:
@@ -62,21 +58,28 @@ class Spaceship(Sprite):
        if self.rect.y > SCREEN_HEIGHT // 2 and self.rect.right <= SCREEN_WIDTH:
          self.rect.y -= self.SHIP_SPEED
          self.rect.x += self.SHIP_SPEED
+       elif self.rect.right >= SCREEN_WIDTH - self.SHIP_HEIGHT:
+         self.rect.x = 0
          
    def move_up_left(self): 
        if self.rect.y > SCREEN_HEIGHT // 2 and self.rect.left >= 0:
          self.rect.y -= self.SHIP_SPEED
-         self.rect.x -= self.SHIP_SPEED      
-
+         self.rect.x -= self.SHIP_SPEED 
+       elif self.rect.left < 0:
+        self.rect.x = SCREEN_WIDTH - self.SHIP_WIDTH
+            
    def move_down_right(self): 
        if self.rect.y < SCREEN_HEIGHT - 70 and self.rect.right <= SCREEN_WIDTH:
          self.rect.y += self.SHIP_SPEED
          self.rect.x += self.SHIP_SPEED
-
+       elif self.rect.right >= SCREEN_WIDTH - self.SHIP_HEIGHT:
+         self.rect.x = 0
    def move_down_left(self): 
        if self.rect.y < SCREEN_HEIGHT - 70 and self.rect.left >= 0:
          self.rect.y += self.SHIP_SPEED
-         self.rect.x -= self.SHIP_SPEED 
+         self.rect.x -= self.SHIP_SPEED
+       elif self.rect.left < 0:
+        self.rect.x = SCREEN_WIDTH - self.SHIP_WIDTH
 
    def draw(self, screen):
        screen.blit(self.image, (self.rect.x, self.rect.y)) 
